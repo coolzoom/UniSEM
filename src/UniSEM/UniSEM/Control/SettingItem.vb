fdd32731d50072d38f57f02c3fe6f4da0ed5ae98
coolzoom <tommingc@gmail.com>
2020-08-21 00:51:46 +0800

    Update SettingItem.vb

--- src/UniSEM/UniSEM/Control/SettingItem.vb
+++ src/UniSEM/UniSEM/Control/SettingItem.vb
@@ -10,8 +10,10 @@
         Try
             System.Environment.SetEnvironmentVariable(ENV_NAME, MetroComboBox1.Text, EnvironmentVariableTarget.Machine)
             lblCurrent.Text = Environment.GetEnvironmentVariable(ENV_NAME)
+            Me.Invalidate()
+            form_alert.Show("设置成功/Success", form_alert.AlertType.success)
         Catch ex As Exception
-            MsgBox(ex.Message)
+            form_alert.Show("设置失败/Fail " & ex.Message, form_alert.AlertType.[error])
         End Try
 
     End Sub
